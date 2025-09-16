34545b82c4034d79703c9271e67ec3c4718cea34
Nishant kumar <nishant201kashyap@gmail.com>
2025-09-16 12:12:18 +0530

    add

---
+++ Number_guessing/Number_gues.py
@@ -0,0 +1,6 @@
+import random
+import time
+while True:
+ number = random.randint(1,100)
+ time.sleep(1)
+ print(number)
---
+++ Number_guessing/Quote_generation.py
@@ -0,0 +1,8 @@
+import random
+import time
+
+while True :
+ Mn = ["Be crazy","Become a master","Chance getter","Hello friends","Be passionate"]
+ time.sleep(0.5)
+
+ print(random.choice(Mn))
